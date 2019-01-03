7ce84d0bc478a650a9f008ffd2926e7db463091f
laurencezabanal <42401130+laurencezabanal@users.noreply.github.com>
2019-01-03 21:03:23 +0800

    Add files via upload

---
+++ template_SLRegression.R
@@ -0,0 +1,71 @@
+# Regression Template
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+
+# Splitting the dataset into the Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+#This package will not need manual feature scaling
+
+
+# Fitting the Regression Model to the dataset
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
+#~ means is proportional
+#check and input "summary(regressor)" in the console
+
+# Create your regressor here
+
+# Predicting a new result
+y_pred = predict(regressor,newdata = test_set)
+
+# Visualising the Regression Model results (Training set)
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Years of Experience (Regression Model)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+
+
+
+# Visualising the Regression Model results (Test set)
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Years of Experience (Regression Model)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+
+
+
+# Visualising the Regression Model results (for higher resolution and smoother curve)
+# install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
